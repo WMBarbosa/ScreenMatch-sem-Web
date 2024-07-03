de0afd9e6f36573813ec66af8bb1c6e6178e9463
WMBarbosa <wesleymario01@gmail.com>
2024-07-03 17:05:59 -0300

    Commit update

--- src/main/java/br/com/alura/ScreenMatch2/ScreenMatch2Application.java
+++ src/main/java/br/com/alura/ScreenMatch2/ScreenMatch2Application.java
@@ -1,8 +1,5 @@
 package br.com.alura.ScreenMatch2;
-
-import br.com.alura.ScreenMatch2.model.DadosSerie;
-import br.com.alura.ScreenMatch2.service.ConsumoApi;
-import br.com.alura.ScreenMatch2.service.ConverteDados;
+import br.com.alura.ScreenMatch2.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +13,8 @@ public class ScreenMatch2Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=515bc55a");
-		System.out.println(json);
-//		json = consumoApi.obterDados("Https://coffee.alexflipnote.dev/random.json");
-//		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
